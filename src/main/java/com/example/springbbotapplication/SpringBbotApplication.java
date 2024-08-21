e777874d25ea2c5e908a63b1e336a9ecebb86da4
gaurav0021 <gauravjoshi0021@gmail.com>
2024-08-21 06:59:23 +0000

    Update SpringBbotApplication.java
    changed the message

--- src/main/java/com/example/springbbotapplication/SpringBbotApplication.java
+++ src/main/java/com/example/springbbotapplication/SpringBbotApplication.java
@@ -13,8 +13,8 @@ public class SpringBbotApplication {
         SpringApplication.run(SpringBbotApplication.class, args);
     }
 
-    @GetMapping("/")
-    public String entry() {
-        return "Welcomes in Spring Boot Application";
+    @GetMapping("/message")
+    public String message() {
+        return "Congrats ! your application deployed successfully in Azure !";
     }
 }
